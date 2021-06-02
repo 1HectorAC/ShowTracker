a381b46926ff1168cab468e15dcc882337334f4c
Hector Acosta <hacostacervantes17@mail.wou.edu>
2021-06-02 14:40:24 -0700

    Fixed spacing error in rundev_example.sh.

--- rundev_example.sh
+++ rundev_example.sh
@@ -2,7 +2,7 @@
 
 export FLASK_APP=app.py
 export FLASK_ENV=development
-export DB_HOST = 'Add connection String here'
-export SECRET_KEY = 'Add secret key here'
+export DB_HOST='Add connection String here'
+export SECRET_KEY='Add secret key here'
 
 flask run --host=0.0.0.0
