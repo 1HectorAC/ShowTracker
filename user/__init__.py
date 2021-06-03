067febcb2ff54aef244aaf4c346d557e36c1891e
Hector Acosta <hacostacervantes17@mail.wou.edu>
2021-06-03 14:44:32 -0700

    Added user related files.

---
+++ user/__init__.py
@@ -0,0 +1,1 @@
+#let us treat user folder as python module we can import into other
---
+++ user/models.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, request, session, redirect
+from passlib.hash import pbkdf2_sha256
+import uuid
+from mongoengine import connect, Document, StringField, ListField, DictField
+import json
+import os
+#from app import client
+
+connect(db="test", host=os.getenv('DB_HOST'), port=27017)
+
+# mongoEngine model for adding to db
+class ShowUser(Document):
+    _id = StringField(required=True)
+    name = StringField(required=True, max_length=64)
+    email = StringField(required=True, max_length=64)
+    password = StringField(required=True)
+    shows = ListField(DictField())
+
+class User:
+
+    def start_session(self, user):
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
+    
+    def signup(self):
+
+        # Create to user object
+        user = ShowUser(
+            _id = uuid.uuid4().hex,
+            name = request.form.get('name'),
+            email = request.form.get('email'),
+            password = request.form.get('password'),
+            shows = []
+        )
+
+        # Encrypt password
+        user.password = pbkdf2_sha256.encrypt(user['password'])
+
+        # Check for existing email address
+        if ShowUser.objects(email=user['email']).count() != 0:
+            return jsonify({"error": "Email address alread in use"}), 400
+
+        # Save user to db and start session. 
+        # Note: passing python dict of user
+        if user.save():
+            return self.start_session(json.loads(user.to_json()))
+
+        #print(user.to_json())
+
+        return jsonify({"error": "Signup failed"}), 400
+
+    def signout(self):
+        session.clear()
+        return redirect('/')
+
+    def login(self):
+        
+        # Check if email match exists
+        if ShowUser.objects(email=request.form.get('email')).count() != 0:
+            user = json.loads(ShowUser.objects.get(email =request.form.get('email')).to_json())
+
+            # Check if password match
+            if pbkdf2_sha256.verify(request.form.get('password'), user['password']): 
+                return self.start_session(user)
+
+        # retur error
+        return jsonify({"error": "Invalid login crdentials"}), 401
