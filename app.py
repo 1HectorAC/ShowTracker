25dd528d9804b07b70f1193f89c5b7c24a733865
Hector Acosta <hacostacervantes17@mail.wou.edu>
2021-06-04 12:47:29 -0700

    Added login related stuff to app.py.

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, redirect, session
+from functools import wraps
+import os
 
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+
+# Decorators. (function will be passed in function (dashboard_render), decide if go though or not)
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs) #return orginial function trying to access
+        else:
+            return redirect('/') #redirect to home page
+    return wrap
+
+#Routes
+from user import routes
 
 @app.route("/", methods = ["GET"])
 def home():
     return render_template("home.html")
+
+@app.route('/dashboard/', methods=["GET"])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
