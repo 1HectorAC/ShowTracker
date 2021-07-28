c0f7680b1b4d6a6eac07d2a498e96f537eb3e899
Hector Acosta <hacostacervantes17@mail.wou.edu>
2021-07-28 13:58:09 -0700

    Added editPassword form handling in scripts.js.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -104,6 +104,27 @@ $("form[name=editEmail]").submit(function(e){
     e.preventDefault();
 });
 
+$("form[name=editPassword]").submit(function(e){
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/user/editPassword",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp) {
+            window.location.href = "/profile/";
+        },
+        error: function(resp){
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
+
 $("form[name=editShow]").submit(function(e){
     var $form = $(this);
     var $error = $form.find(".error");
